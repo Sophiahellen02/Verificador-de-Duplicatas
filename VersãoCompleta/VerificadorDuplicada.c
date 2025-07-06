d65594ec53b923ff407e2237b97bf0fe500de20d
Sophia Hellen <sophiahellen0212@icloud.com>
2025-07-06 09:08:25 -0300

    Adição de constantes e estruturas para a tabela hash

--- VersãoCompleta/VerificadorDuplicada.c
+++ VersãoCompleta/VerificadorDuplicada.c
@@ -2,3 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#define TAM_MAX_LISTA 10000
+#define TAM_MAX_LINHA 200
+
+typedef struct No{
+    char *str;
+    struct No *prox;
+} No;
+
+typedef struct TabelaHash{
+    No **listas;
+    int tamanho;
+} TabelaHash;
