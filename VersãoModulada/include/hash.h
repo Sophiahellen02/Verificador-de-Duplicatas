d5fba78029c722a1b1adc96fe7ebb7a04bfbe72f
Sophia Hellen <sophiahellen0212@icloud.com>
2025-07-08 10:23:49 -0300

    Adiciona arquivo de cabeçalho hash.h com estruturas e funções da tabela hash

---
+++ VersãoModulada/include/hash.h
@@ -0,0 +1,20 @@
+#ifndef HASH_H
+#define HASH_H
+
+typedef struct{
+    char *str;
+    int contagem;
+    struct No *prox;
+} No;
+
+typedef struct TabelaHash{
+    No **tabela;
+    int tamanho;
+} TabelaHash;
+
+TabelaHash *criar_tabela_hash(int tamanho);
+void liberar_tabela_hash(TabelaHash *tabela);
+int inserir_tabela_hash(TabelaHash *tabela, const char *str);
+void imprime_duplicatas(TabelaHash *tabela);
+
+#endif
