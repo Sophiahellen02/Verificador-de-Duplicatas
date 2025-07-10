a5dcac3363569efc09b83dac6ec31b3dfe793573
Sophia Hellen <sophiahellen0212@icloud.com>
2025-07-10 10:19:41 -0300

    Adição do código do arquivo verificador.c, incluindo as funções de comparar strings, verificar se a lista está ordenada e verificar duplicatas linearmente.

---
+++ VersãoModulada/src/verificador.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include "verificador.h"
+
+int comparar_strings(const void *a, const void *b){
+    char * const *sa = a;
+    char * const *sb = b;
+    return strcmp(*sa, *sb);
+}
+
+int verifica_ordenado(char **linhas, int n){
+    qsort(linhas, n, sizeof(char *), comparar_strings);
+    for (int i = 1; i < n; i++{
+        if (strcmp(linhas[i - 1], linhas[i]) == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int verifica_linear(char **linhas, int n){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if (strcmp(linhas[i], linhas[j]) == 0){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
