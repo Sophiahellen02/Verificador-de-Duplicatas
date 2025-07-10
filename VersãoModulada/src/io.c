eaa0c7e31da5f3e4a9e0270741a9dc5d4a4cdd4f
Sophia Hellen <sophiahellen0212@icloud.com>
2025-07-10 10:06:40 -0300

    adição do código do arquivo io.c que implementa as funções de entrada e saída do programa

---
+++ VersãoModulada/src/io.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "io.h"
+
+void menu(void){
+    printf("\n============================\n");
+    printf(" VERIFICADOR DE DUPLICATAS\n");
+    printf("============================\n");
+    printf("1. Inserir manualmente\n");
+    printf("2. Importar CSV\n");
+    printf("0. Sair\n");
+    printf("============================\n");
+    printf("Escolha: ");
+}
